b5002f2404917ccd28a7cc35358406c847a5b78c
ryuchanghwi <ckdgnl10@naver.com>
2024-01-18 01:21:24 +0900

    [Feature] : 숫자에 콤마 찍기 기능 추가, 테스트 코드 작성

--- ExchangeRateCalcApp/ExchangeRateCalcApp/CalculationWork/NumberWork.swift
+++ ExchangeRateCalcApp/ExchangeRateCalcApp/CalculationWork/NumberWork.swift
@@ -12,5 +12,13 @@ struct NumberWork {
         let multiplier = pow(10.0, Double(point))
         return Double(Int(value * multiplier)) / multiplier
     }
-    
+    static func addComma(_ value: Double) -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        return numberFormatter.string(from: NSNumber(value: value)) ?? ""
+    }
+    /*
+     콤마기능 참고
+     https://velog.io/@juh2/Swift-%EC%88%AB%EC%9E%90%EC%97%90-%EC%BD%A4%EB%A7%88-%EB%84%A3%EA%B8%B0-%EB%B9%BC%EA%B8%B0-%EA%B0%80%EA%B2%A9-%ED%91%9C%EC%8B%9C
+     */
 }
