001df59f6a092da39f2117c4a1da40a50f40b357
ryuchanghwi <ckdgnl10@naver.com>
2024-01-17 18:32:27 +0900

    [FEAT] : 소수점첫번째자리까지있는 값이 주어졌을떄 두번째 소수점이 0이 나오는지 확인하는 테스트 코드 추가

--- ExchangeRateCalcApp/ExchangeRateCalcAppTests/CalculationWorkTests/DecimalPointWorkTests.swift
+++ ExchangeRateCalcApp/ExchangeRateCalcAppTests/CalculationWorkTests/DecimalPointWorkTests.swift
@@ -18,10 +18,19 @@ final class DecimalPointWorkTests: XCTestCase {
         // Then
         XCTAssertEqual(returnValue, expectedValue)
     }
-    func test_truncateDecimal_소주가없는값이주어졌을때_소수두번째값까지0이이나오는지확인() {
+    func test_truncateDecimal_소수가없는값이주어졌을때_소수두번째값까지0이이나오는지확인() {
         // Given
         let value:Double = 123
         let expectedValue = 123.00
+        // When
+        let returnValue = DecimalPointWork.truncateDecimal(value, point: 2)
+        // Then
+        XCTAssertEqual(returnValue, expectedValue)
+    }
+    func test_truncateDecimal_소수점첫번째자리까지값이주어졌을때_소수두번째값에0이이나오는지확인() {
+        // Given
+        let value:Double = 123.1
+        let expectedValue = 123.10
         // When
         let returnValue = DecimalPointWork.truncateDecimal(value, point: 2)
         // Then
