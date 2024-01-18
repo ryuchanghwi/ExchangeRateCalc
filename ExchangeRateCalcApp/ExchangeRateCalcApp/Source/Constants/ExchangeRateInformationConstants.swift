4c938101e7ddc622c408e1c3cc33d475f17204d9
ryuchanghwi <ckdgnl10@naver.com>
2024-01-18 21:14:21 +0900

    [Feature] : ExchangeRateInformationConstants에 dummyData 추가

---
+++ ExchangeRateCalcApp/ExchangeRateCalcApp/Source/Constants/ExchangeRateInformationConstants.swift
@@ -0,0 +1,12 @@
+//
+//  ExchangeRateInformationConstants.swift
+//  ExchangeRateCalcApp
+//
+//  Created by 류창휘 on 1/18/24.
+//
+
+import Foundation
+
+struct ExchangeRateInformationConstants {
+    static let dummyData = ExchangeRateInformationDTO(succes: true, terms: "https://currencylayer.com/terms", privacy: "https://currencylayer.com/privacy", timestamp: 1705572303, source: "USD", quotes: Quotes(koreaExChangeRate: 320.489701, japenExChangeRate: 147.811026, philippinesChangeRate: 55.828506))
+}
