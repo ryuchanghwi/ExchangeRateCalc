9f3f76fe0dd1ef7a2a4d8bd48bbeae741a381d3d
ryuchanghwi <ckdgnl10@naver.com>
2024-01-19 16:45:07 +0900

    [Feature] : MockURLProtocol 구현

--- ExchangeRateCalcApp/ExchangeRateCalcApp/Network/Base/MockURLProtocol.swift
+++ ExchangeRateCalcApp/ExchangeRateCalcApp/Network/Base/MockURLProtocol.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+/*
+ 해당 코드 참고 - https://samwize.com/2022/07/07/how-to-use-urlprotocol-to-mock-networking-api/
+ */
+
+final class MockURLProtocol: URLProtocol {
+    static var stubResponseData: Data?
+    static var error: Error?
+    override class func canInit(with request: URLRequest) -> Bool {
+        true
+    }
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        request
+    }
+    override func startLoading() {
+        if let error = MockURLProtocol.error {
+            self.client?.urlProtocol(self, didFailWithError: error)
+        } else {
+            self.client?.urlProtocol(self, didLoad: MockURLProtocol.stubResponseData ?? Data())
+        }
+        self.client?.urlProtocolDidFinishLoading(self)
+    }
+    override func stopLoading() {
+    }
+}
