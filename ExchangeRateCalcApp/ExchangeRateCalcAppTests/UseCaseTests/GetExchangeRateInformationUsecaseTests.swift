ccc14eccef71d4247e3f9ab146794e4e7720af52
ryuchanghwi <ckdgnl10@naver.com>
2024-01-19 14:06:24 +0900

    [Test] : GetExchangeRateInformationUsecaseTests 테스트 코드 작성

--- ExchangeRateCalcApp/ExchangeRateCalcAppTests/UseCaseTests/GetExchangeRateInformationUsecaseTests.swift
+++ ExchangeRateCalcApp/ExchangeRateCalcAppTests/UseCaseTests/GetExchangeRateInformationUsecaseTests.swift
@@ -6,30 +6,83 @@
 //
 
 import XCTest
+import Combine
+@testable import ExchangeRateCalcApp
 
-final class GetExchangeRateInformationUsecaseTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+struct MockExchangeRateinformationRepository: ExchangeRateinformationRepositoryInterface {
+    let hasError: Bool
+    init(hasError: Bool) {
+        self.hasError = hasError
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func data() -> AnyPublisher<ExchangeRateInformationDTO, ErrorTypes> {
+        if hasError {
+            return Fail(error: ErrorTypes.noData)
+                .eraseToAnyPublisher()
+        } else {
+            let exchangeRateData = ExchangeRateInformationConstants.dummyData
+            return Just(exchangeRateData)
+                .setFailureType(to: ErrorTypes.self)
+                .eraseToAnyPublisher()
         }
     }
+}
 
+final class GetExchangeRateInformationUsecaseTests: XCTestCase {
+    var sut: GetExchangeRateInformationUsecase!
+    var cancellables: Set<AnyCancellable>!
+    override func setUpWithError() throws {
+        cancellables = []
+    }
+    override func tearDownWithError() throws {
+        sut = nil
+        cancellables = nil
+    }
+    func test_usecase_데이터가주어졌을때_데이터가출력되는지확인() {
+        // Given
+        sut = GetExchangeRateInformationUsecase(exchangeRateInformationRepository: MockExchangeRateinformationRepository(hasError: false))
+        let expectedValue = ExchangeRateInformationConstants.dummyData
+        let expectation = self.expectation(description: "데이터 조회 성공")
+        // When
+        var returnValue: ExchangeRateInformationDTO?
+        sut.execute()
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    return
+                case .failure(let error):
+                    XCTFail("테스트 실패 \(error)")
+                }
+            } receiveValue: { data in
+                returnValue = data
+                expectation.fulfill()
+            }.store(in: &cancellables)
+        // Then
+        self.wait(for: [expectation], timeout: 5)
+        XCTAssertEqual(returnValue?.quotes.japenExChangeRate, expectedValue.quotes.japenExChangeRate)
+        XCTAssertEqual(returnValue?.quotes.koreaExChangeRate, expectedValue.quotes.koreaExChangeRate)
+        XCTAssertEqual(returnValue?.quotes.philippinesChangeRate, expectedValue.quotes.philippinesChangeRate)
+    }
+    func test_usecase_에러상황이주어졌을때_에러를출력되는지확인() {
+        // Given
+        sut = GetExchangeRateInformationUsecase(exchangeRateInformationRepository: MockExchangeRateinformationRepository(hasError: true))
+        let expectationValue: ErrorTypes = .noData
+        let expectation = self.expectation(description: "noData 에러 발생")
+        // When
+        var returnValue: ErrorTypes?
+        sut.execute()
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    return
+                case .failure(let error):
+                    returnValue = error
+                    expectation.fulfill()
+                }
+            } receiveValue: { data in
+                XCTFail("테스트 실패 \(data)")
+            }.store(in: &cancellables)
+        // Then
+        self.wait(for: [expectation], timeout: 5)
+        XCTAssertEqual(returnValue, expectationValue)
+    }
 }
