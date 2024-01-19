100a93bce265bc6ec7af07d132b02293a4027bdc
ryuchanghwi <ckdgnl10@naver.com>
2024-01-19 16:51:53 +0900

    [Feature] : ExchangeRateInformationConstants jsonString 데이터 추가

--- ExchangeRateCalcApp/ExchangeRateCalcApp/Source/Constants/ExchangeRateInformationConstants.swift
+++ ExchangeRateCalcApp/ExchangeRateCalcApp/Source/Constants/ExchangeRateInformationConstants.swift
@@ -9,4 +9,18 @@ import Foundation
 
 struct ExchangeRateInformationConstants {
     static let dummyData = ExchangeRateInformationDTO(succes: true, terms: "https://currencylayer.com/terms", privacy: "https://currencylayer.com/privacy", timestamp: 1705572303, source: "USD", quotes: Quotes(koreaExChangeRate: 320.489701, japenExChangeRate: 147.811026, philippinesChangeRate: 55.828506))
+    static let jsonString = """
+{
+    "success": true,
+    "terms": "https://currencylayer.com/terms",
+    "privacy": "https://currencylayer.com/privacy",
+    "timestamp": 1705572303,
+    "source": "USD",
+    "quotes": {
+        "USDKRW": 3.67288,
+        "USDJPY": 72.820546,
+        "USDPHP": 95.306269
+}
+
+"""
 }
