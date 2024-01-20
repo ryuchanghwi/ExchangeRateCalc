aeba8338e335eb8a76ca5608b5267d9db3913df9
ryuchanghwi <ckdgnl10@naver.com>
2024-01-20 17:39:49 +0900

    [Feature] ExchangeRateinformationRepository 구현 및 테스트

--- ExchangeRateCalcApp/ExchangeRateCalcApp/Data/Repository/ExchangeRateinformationRepository.swift
+++ ExchangeRateCalcApp/ExchangeRateCalcApp/Data/Repository/ExchangeRateinformationRepository.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+import Combine
+
+struct ExchangeRateinformationRepository: ExchangeRateinformationRepositoryInterface {
+    private let service: GetService
+    init(service: GetService) {
+        self.service = service
+    }
+    func data() -> AnyPublisher<ExchangeRateInformationDTO, ErrorTypes> {
+        self.service.request(from: "\(Secrets.baseURL)live?access_key=\(Secrets.apiKey)")
+    }
+}
