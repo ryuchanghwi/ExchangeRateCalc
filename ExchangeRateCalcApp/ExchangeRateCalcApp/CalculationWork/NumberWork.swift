5fc3ee57aefafdd3ed690a36c0cd848520a86193
ryuchanghwi <ckdgnl10@naver.com>
2024-01-22 01:33:25 +0900

    [Feature] : checkReceptionAmountState 메서드 구현 및 테스트

--- ExchangeRateCalcApp/ExchangeRateCalcApp/CalculationWork/NumberWork.swift
+++ ExchangeRateCalcApp/ExchangeRateCalcApp/CalculationWork/NumberWork.swift
@@ -6,7 +6,10 @@
 //
 
 import Foundation
-
+/*
+ 콤마기능 참고
+ https://velog.io/@juh2/Swift-%EC%88%AB%EC%9E%90%EC%97%90-%EC%BD%A4%EB%A7%88-%EB%84%A3%EA%B8%B0-%EB%B9%BC%EA%B8%B0-%EA%B0%80%EA%B2%A9-%ED%91%9C%EC%8B%9C
+ */
 struct NumberWork {
     static func truncateDecimal(_ value: Double, point: Int) -> Double {
         let multiplier = pow(10.0, Double(point))
@@ -17,8 +20,11 @@ struct NumberWork {
         numberFormatter.numberStyle = .decimal
         return numberFormatter.string(from: NSNumber(value: value)) ?? ""
     }
-    /*
-     콤마기능 참고
-     https://velog.io/@juh2/Swift-%EC%88%AB%EC%9E%90%EC%97%90-%EC%BD%A4%EB%A7%88-%EB%84%A3%EA%B8%B0-%EB%B9%BC%EA%B8%B0-%EA%B0%80%EA%B2%A9-%ED%91%9C%EC%8B%9C
-     */
+    static func checkReceptionAmountState(amount: Double) -> Bool {
+        var returnValue: Bool = true
+        if amount < 0 || amount > 10000 {
+            returnValue = false
+        }
+        return returnValue
+    }
 }
